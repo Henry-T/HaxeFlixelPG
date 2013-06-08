03407d8924afce05469f108415fe5b333f886495
henry <tang.hy@live.com>
2013-06-08 10:15:58 +0800

    mod CameraAlpha改名为CameraBgAlpha并作了补充记录

--- Graphic/CameraBgAlpha/source/MenuState.hx
+++ Graphic/CameraBgAlpha/source/MenuState.hx
@@ -23,7 +23,10 @@ class MenuState extends FlxState
 	override public function create():Void
 	{
 		FlxG.bgColor = 0xff000000;
+		// This is the last effect you may want
 		FlxG.camera.bgColor = 0x00000000;
+		// But this is a fancy blur effect
+		FlxG.camera.bgColor = 0x21000000;
 
 		sprite = new FlxSprite(0,0);
 		add(sprite);
@@ -39,11 +42,14 @@ class MenuState extends FlxState
 
 	override public function draw():Void {
 		// # Uncomment this line to clear camera buffer to transparent
+		// # Solution: This works if you don't want blend effect!
 		// -----------------------------------------------------------
 		// FlxG.camera.buffer.fillRect(new flash.geom.Rectangle(0,0,FlxG.width,FlxG.height), 0x00000000);
 
 
 		// # This method is used inside FlxCamera, but it doesn't work
+		// # Answer: FlxCamera is designed to blend two fill instead of replacing the old one
+		// # Answer: set last param(mergeAlpha) of copyPixels does the magic
 		// # ---------------------------------------------------------
 		//var _fill:flash.display.BitmapData = new flash.display.BitmapData(FlxG.width,FlxG.height, true, 0x00000000);
 		//_fill.fillRect(new flash.geom.Rectangle(0,0,640, 480), 0x00000000);
