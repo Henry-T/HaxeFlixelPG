f9b879aa837708f60e9478c6f0e978b37c308f0c
henry <tang.hy@live.com>
2013-07-19 10:34:09 +0800

    mod generate return value for build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 files=`find . -name *.hxproj`
+ret=0
 for f in $files; do
   full=`readlink -f $f`
   projFile=${full%%.*}.xml
   echo TEST ON $projFile
   haxelib run openfl build "$projFile" flash
+  curRet=$?
+  if [ $curRet -ne 0 ]
+  then ret=1
+  else echo OK
+  fi
 done
+exit $ret
