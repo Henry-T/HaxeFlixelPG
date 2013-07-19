9a8427c8a46b58d85f3ef254c836096067fcecd7
henry <tang.hy@live.com>
2013-07-19 13:45:52 +0800

    mod test native build in travis script

--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
+function buildFlash(){
+	echo Test On Flash $projFile
+	haxelib run openfl build "$projFile" flash
+	return $?
+}
+
+function buildCPP(){
+  echo Test On CPP $projFile
+	haxelib run openfl build "$projFile" cpp
+	return $?
+}
+
 files=`find . -name *.hxproj`
 ret=0
+projFile=""
 for f in $files; do
   full=`readlink -f $f`
   projFile=${full%%.*}.xml
-  echo TEST ON $projFile
-  haxelib run openfl build "$projFile" flash
-  curRet=$?
-  if [ $curRet -ne 0 ]
-  then ret=1
-  else echo OK
-  fi
+  buildFlash
+  if [ $? -ne 0 ]; then ret=1; fi
+  buildCPP
+  if [ $? -ne 0 ]; then ret=1; fi
 done
 exit $ret
+
